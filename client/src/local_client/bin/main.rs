034a944feff256b45081e21f854892c9ece55db7
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-14 10:25:57 +0200

    importing Game triggers the error

--- client/src/local_client/bin/main.rs
+++ client/src/local_client/bin/main.rs
@@ -1,15 +1,17 @@
+use macroquad::prelude::*;
 use server::game::Game;
-
-use client::local_client;
 
 #[macroquad::main("Clash")]
 async fn main() {
-    //todo add button to decide random or fixed game
-    let game = if true {
-        Game::new(2, "c".repeat(32), true)
-    } else {
-        local_client::setup_local_game()
-    };
+    loop {
+        clear_background(RED);
 
-    local_client::run(game).await;
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("IT WORKS! 3", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,13 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("Clash")]
 async fn main() {
-    loop {
-        clear_background(RED);
+    //todo add button to decide random or fixed game
+    let game = if true {
+        Game::new(2, "c".repeat(32), true)
+    } else {
+        local_client::setup_local_game()
+    };
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-
-        draw_text("IT WORKS! 3", 20.0, 20.0, 30.0, DARKGRAY);
-
-        next_frame().await
-    }
+    local_client::run(game).await;
 }
