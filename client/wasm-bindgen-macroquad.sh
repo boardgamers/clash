5396c38e2ec6d57c7d366d328ea28f3225f44ac1
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-16 14:35:43 +0200

    enable import/export in native client

--- client/wasm-bindgen-macroquad.sh
+++ client/wasm-bindgen-macroquad.sh
@@ -114,10 +114,10 @@ TARGET_DIR="target/wasm32-unknown-unknown"
 # Build
 echo "Building $PROJECT_NAME..."
 if [ -n "$RELEASE" ]; then
-	cargo build --release --target wasm32-unknown-unknown
+	cargo build --release --target wasm32-unknown-unknown --features "wasm"
 	TARGET_DIR="$TARGET_DIR/release"
 else
-	cargo build --target wasm32-unknown-unknown
+	cargo build --target wasm32-unknown-unknown --features "wasm"
 	TARGET_DIR="$TARGET_DIR/debug"
 fi
 
