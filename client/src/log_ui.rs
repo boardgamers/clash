1c781a94bef9fad454158a2b9e9b6ab0cd46e0a6
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-10-03 12:24:54 +0200

    multiline log

--- client/src/log_ui.rs
+++ client/src/log_ui.rs
@@ -1,3 +1,4 @@
+use macroquad::ui::Ui;
 use server::game::Game;
 
 use crate::client_state::StateUpdate;
@@ -6,9 +7,22 @@ use crate::dialog_ui::closeable_dialog_window;
 pub fn show_log(game: &Game) -> StateUpdate {
     closeable_dialog_window("Log", |ui| {
         game.log.iter().for_each(|l| {
-            ui.label(None, l);
+            multiline(ui, l);
         });
-
         StateUpdate::None
     })
+}
+
+fn multiline(ui: &mut Ui, text: &str) {
+    let mut line = String::new();
+    text.split(' ').for_each(|s| {
+        if line.len() + s.len() > 100 {
+            ui.label(None, &line);
+            line = String::new();
+        }
+        line.push_str(s);
+    });
+    if !line.is_empty() {
+        ui.label(None, &line);
+    }
 }
