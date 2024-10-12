4606daf967edc6ee58ea6b2b79e3271d4016c78c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-10-12 15:00:45 +0200

    glcanvas is already present

--- client/js/src/run.js
+++ client/js/src/run.js
@@ -9,32 +9,24 @@ function dynamicallyLoadScript(url, onload) {
 }
 
 export async function run({selector, control}) {
-    const root = document.querySelector(selector);
-    const canvas = document.createElement("canvas");
-    canvas.setAttribute("id", "glcanvas");
-    canvas.setAttribute("style", ` 
-                margin: 0px;
-                padding: 0px;
-                width: 100%;
-                height: 100%;
-                overflow: hidden;
-                position: absolute;
-                z-index: 0;
-    `);
-    root.appendChild(canvas);
-
-    dynamicallyLoadScript("https://not-fl3.github.io/miniquad-samples/mq_js_bundle.js", async () => {
         let wbg = await init();
-        miniquad_add_plugin({
-            register_plugin: (a) => (a.wbg = wbg),
-            on_init: () => set_wasm(wasm_exports),
-            version: "0.0.1",
-            name: "wbg",
-        });
-        const src = document.head.getElementsByTagName("script")[0].src;
-        control.assets_url = src.replace("client.js", "assets/");
-        const url = src.replace("client.js", "client.wasm");
-        console.log("Loading wasm from", url);
-        await load(url);
-    });
+         miniquad_add_plugin({
+             register_plugin: (a) => {
+                 console.log("register_plugin", a);
+                 return (a.wbg = wbg)
+             },
+             on_init: () => {
+                 console.log("on_init", wasm_exports);
+                 window.clash_control.send_ready()
+                 return set_wasm(wasm_exports)
+             },
+             version: "0.0.1",
+             name: "wbg",
+         });
+         const src = document.body.getElementsByTagName("script")[1].src;
+         control.assets_url = src.replace("client.js", "assets/");
+         const url = src.replace("client.js", "client.wasm");
+         console.log("Loading wasm from", url);
+         await load(url);
+         console.log("Loaded wasm");
 }
