bec0b9e2628cd7e42cc74855e2598aceabf26b07
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-26 18:10:41 +0100

    update deps (#89)
    * update deps
    
    * update deps
    
    * update deps
    
    * update deps

--- scripts/build-remote-client.sh
+++ scripts/build-remote-client.sh
@@ -73,8 +73,10 @@ echo "Patching wasm-bindgen output..."
 
 # Shim to tie the thing together
 sed -i "s/import \* as __wbg_star0 from 'env';//" dist/"$PROJECT_NAME".js
+sed -i "s/import \* as __wbg_star1 from 'env';//" dist/"$PROJECT_NAME".js
 sed -i "s/let wasm;/let wasm; export const set_wasm = (w) => wasm = w;/" dist/"$PROJECT_NAME".js
 sed -i "s/imports\['env'\] = __wbg_star0;/return imports.wbg\;/" dist/"$PROJECT_NAME".js
+sed -i "s/imports\['env'\] = __wbg_star1;/return imports.wbg\;/" dist/"$PROJECT_NAME".js
 sed -i "s/const imports = __wbg_get_imports();/return __wbg_get_imports();/" dist/"$PROJECT_NAME".js
 
 pushd js
