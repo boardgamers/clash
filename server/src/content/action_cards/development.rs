d9c38dddb77701fee4ca44d2ce4bd2c61cab7b94
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-03-29 08:08:17 +0100

    mercenary

--- server/src/content/action_cards/development.rs
+++ server/src/content/action_cards/development.rs
@@ -1,7 +1,10 @@
 use crate::action_card::ActionCard;
 use crate::content::action_cards::mercenaries::mercenaries;
-use crate::content::tactics_cards::for_the_people;
+use crate::content::tactics_cards::{for_the_people, heavy_resistance};
 
 pub(crate) fn development_action_cards() -> Vec<ActionCard> {
-    vec![mercenaries(13, for_the_people)]
+    vec![
+        mercenaries(13, for_the_people),
+        mercenaries(14, heavy_resistance),
+    ]
 }
