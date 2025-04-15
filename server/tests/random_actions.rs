998bc66fa3ed2aca559ce0cffcaaa9f99eba749a
AlphaCodingPilot <simon.zeitlinger2@gmail.com>
2025-04-15 16:00:08 +0200

    wip

--- server/tests/random_actions.rs
+++ server/tests/random_actions.rs
@@ -1,3 +1,4 @@
+use async_std::task;
 use server::{
     action::{self, Action},
     ai_actions,
@@ -7,31 +8,50 @@ use server::{
     utils::{Rng, Shuffle},
 };
 
-const ITERATIONS: usize = 10;
+const ITERATIONS: usize = 96;
+const PARALLELIZATION: usize = 24;
 
-#[test]
-fn test_random_actions() {
+#[tokio::test]
+async fn test_random_actions() {
     let mut rng = Rng::new();
-    for _ in 0..ITERATIONS {
-        //todo: multithreading
-        let seed = rng.range(0, 10_usize.pow(15)).to_string();
-        let mut game = game_setup::setup_game(2, seed, true);
-        game.supports_undo = false;
-        loop {
-            if matches!(game.state, GameState::Finished) {
-                break;
-            }
-            let player_index = game.active_player();
-            //todo: include movement action
-            let actions = ai_actions::get_available_actions(&game);
-            let actions = actions
-                .into_iter()
-                .flat_map(|(_, actions)| actions)
-                .collect::<Vec<Action>>();
-            let action = actions
-                .take_random_element(&mut rng)
-                .unwrap_or(Action::Playing(PlayingAction::EndTurn));
-            game = action::execute_action(game, action, player_index);
+    let mut iterations = 0;
+    loop {
+        let mut handles = Vec::new();
+        for _ in 0..PARALLELIZATION {
+            rng.seed = rng.seed.wrapping_add(1);
+            rng.next_seed();
+            let thread_rng = rng.clone();
+            let handle = task::spawn(async move { random_actions_iterations(thread_rng) });
+            handles.push(handle);
         }
+        for handle in handles {
+            handle.await;
+        }
+        iterations += PARALLELIZATION;
+        if iterations >= ITERATIONS {
+            break;
+        }
+    }
+}
+
+fn random_actions_iterations(mut rng: Rng) {
+    let seed = rng.range(0, 10_usize.pow(15)).to_string();
+    let mut game = game_setup::setup_game(2, seed, true);
+    game.supports_undo = false;
+    loop {
+        if matches!(game.state, GameState::Finished) {
+            break;
+        }
+        let player_index = game.active_player();
+        //todo: include movement action
+        let actions = ai_actions::get_available_actions(&game);
+        let actions = actions
+            .into_iter()
+            .flat_map(|(_, actions)| actions)
+            .collect::<Vec<Action>>();
+        let action = actions
+            .take_random_element(&mut rng)
+            .unwrap_or(Action::Playing(PlayingAction::EndTurn));
+        game = action::execute_action(game, action, player_index);
     }
 }
