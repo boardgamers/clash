04b0a201aa5f4130d2891a8f3528e7d29774c577
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-05-03 11:35:36 +0200

    test civs

--- server/src/content/civilizations.rs
+++ server/src/content/civilizations.rs
@@ -14,58 +14,27 @@ pub fn get_all() -> Vec<Civilization> {
         Civilization::new(BARBARIANS, vec![], vec![]),
         Civilization::new(PIRATES, vec![], vec![]),
         // until the real civilizations are implemented
-        Civilization::new(
-            "Federation",
-            vec![],
-            vec![
-                Leader::builder("James T. Kirk", "", "", "", "").build(),
-                Leader::builder("Kathryn Janeway", "", "", "", "").build(),
-                Leader::builder("Spock", "", "", "", "").build(),
-            ],
-        ),
-        Civilization::new(
-            "Borg",
-            vec![],
-            vec![
-                Leader::builder("Borg Queen", "", "", "", "").build(),
-                Leader::builder("Seven of Nine", "", "", "", "").build(),
-                Leader::builder("Jean Luc Picard", "", "", "", "").build(),
-            ],
-        ),
-        Civilization::new(
-            "Klingons",
-            vec![],
-            vec![
-                Leader::builder("Khan", "", "", "", "").build(),
-                Leader::builder("Kahless", "", "", "", "").build(),
-                Leader::builder("Worf", "", "", "", "").build(),
-            ],
-        ),
-        Civilization::new(
-            "Romulans",
-            vec![],
-            vec![
-                Leader::builder("Sela", "", "", "", "").build(),
-                Leader::builder("Narek", "", "", "", "").build(),
-                Leader::builder("Tomalak", "", "", "", "").build(),
-            ],
-        ),
+        Civilization::new("Federation", vec![], vec![
+            Leader::builder("James T. Kirk", "", "", "", "").build(),
+            Leader::builder("Kathryn Janeway", "", "", "", "").build(),
+            Leader::builder("Spock", "", "", "", "").build(),
+        ]),
+        Civilization::new("Borg", vec![], vec![
+            Leader::builder("Borg Queen", "", "", "", "").build(),
+            Leader::builder("Seven of Nine", "", "", "", "").build(),
+            Leader::builder("Jean Luc Picard", "", "", "", "").build(),
+        ]),
+        Civilization::new("Klingons", vec![], vec![
+            Leader::builder("Khan", "", "", "", "").build(),
+            Leader::builder("Kahless", "", "", "", "").build(),
+            Leader::builder("Worf", "", "", "", "").build(),
+        ]),
+        Civilization::new("Romulans", vec![], vec![
+            Leader::builder("Sela", "", "", "", "").build(),
+            Leader::builder("Narek", "", "", "", "").build(),
+            Leader::builder("Tomalak", "", "", "", "").build(),
+        ]),
     ]
-}
-
-fn test0() -> Civilization {
-    Civilization::new(
-        "test0",
-        vec![],
-        vec![
-            Leader::builder("Alexander", "", "", "", "").build(),
-            Leader::builder("Kleopatra", "", "", "", "").build(),
-        ],
-    )
-}
-
-fn test1() -> Civilization {
-    Civilization::new("test1", vec![], vec![])
 }
 
 fn maya() -> Civilization {
@@ -119,9 +88,14 @@ fn maya() -> Civilization {
 #[must_use]
 pub fn get_civilization(name: &str) -> Option<Civilization> {
     match name {
-        "Maya" => Some(maya()),   // still needs to be implemented
-        "test0" => Some(test0()), // for testing
-        "test1" => Some(test1()), // for testing
+        "Maya" => Some(maya()), // still needs to be implemented
+        // for integration testing
+        "test0" => Some(Civilization::new("test0", vec![], vec![
+            Leader::builder("Alexander", "", "", "", "").build(),
+            Leader::builder("Kleopatra", "", "", "", "").build(),
+        ])), // for testing
+        "test1" => Some(Civilization::new("test1", vec![], vec![])), 
+        "test2" => Some(Civilization::new("test2", vec![], vec![])), 
         _ => get_all()
             .into_iter()
             .find(|civilization| civilization.name == name),
